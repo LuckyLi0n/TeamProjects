42fd49b897fd9bebc1e3bdf84a7d24cc9f7c0808
LuckyLi0n <Rita8996@mail.ru>
2018-12-04 00:44:00 +0300

    Вторая версия функции Мув для движущеся пушки.

--- Move.py
+++ Move.py
@@ -1,58 +1,86 @@
 from tkinter import *
 from math import *
-import time
+import random
 
 root = Tk()
 root.geometry('1000x600')
 canv = Canvas(root,  bg='white')
 canv.pack(fill=BOTH, expand=1)
 
-
-
 for circle_right, circle_up, circle_left, circle_down in [(-300, 500, 300, 1100),
-                                                          (600, 400, 1400, 1200), (-50, 425, 950, 1425)]:
+                                                          (600, 400, 1400, 1200),
+                                                          (-50, 425, 950, 1425)]:
     canv.create_oval(circle_right, circle_up, circle_left, circle_down, fill="green", outline='green')
 
 
+turn_point_1, turn_point_2, turn_point_3, turn_point_4 = 0, 139, 727, 1000
 
-global x, y, dx, dy
+global x, y, dx, dy, oval, coordinate_now, coordinate_desired
+
+coordinate_now = random.randint(0, 1000)
+coordinate_desired = random.randint(0, 1000)
 dx = 1
 dy = 1
-y = 500
-x = 0
-oval = canv.create_oval(x - 20, y - 20, x + 20, y + 20, fill="red", outline='red')
-line = canv.create_line(x , y, x + 30, y - 20, width=5, fill="red")
+x = coordinate_now
+y = 0
+
+
+def find_y():
+    global x, y
+    if turn_point_1 <= x <= turn_point_2:
+        y = 800 - sqrt(300 ** 2 - x ** 2)
+    elif turn_point_2 <= x <= turn_point_3:
+        y = 925 - sqrt(500 ** 2 - (x - 450) ** 2)
+    elif turn_point_3 <= x <= turn_point_4:
+        y = 800 - sqrt(400 ** 2 - (x - 1000) ** 2)
+
+
+def coordination():
+    global x, y, dx, coordinate_now, coordinate_desired, line, max, min
+    if coordinate_now >= coordinate_desired:
+        dx = -dx
+        canv.delete(line)
+        line = canv.create_line(x, y, x - 30, y - 20, width=5, fill="red")
+        max = coordinate_now
+        min = coordinate_desired
+    else:
+        dx = dx
+        max = coordinate_desired
+        min = coordinate_now
 
 
 def move():
-    def flight():
-        global x, y, dx, dy
-        if x in range(0, 400):
-            """Вторая координата определяется по нажатию мышки"""
+    def run():
+        global x, y, dx, dy, oval, coordinate_now, coordinate_desired
+        if min <= x <= max:
             x = x + dx
-            if x in range(0, 138):
+            if turn_point_1 <= x <= turn_point_2:
                 f = 800 - sqrt(300 ** 2 - x ** 2)
                 dy = f - y
                 y = y + dy
                 canv.move(oval, dx, dy)
                 canv.move(line, dx, dy)
-            elif x in range(138, 727):
+            elif turn_point_2 <= x <= turn_point_3:
                 f = 925 - sqrt(500 ** 2 - (x - 450) ** 2)
                 dy = f - y
                 y = y + dy
                 canv.move(oval, dx, dy)
                 canv.move(line, dx, dy)
-            elif x in range(727, 1000):
+            elif turn_point_3 <= x <= turn_point_4:
                 f = 800 - sqrt(400 ** 2 - (x - 1000) ** 2)
                 dy = f - y
                 y = y + dy
                 canv.move(oval, dx, dy)
                 canv.move(line, dx, dy)
 
-        root.after(20, flight)
-    root.after(20, flight())
+        root.after(30, run)
+    root.after(20, run())
 
 
+find_y()
+oval = canv.create_oval(x - 20, y - 20, x + 20, y + 20, fill="red", outline='red')
+line = canv.create_line(x, y, x + 30, y - 20, width=5, fill="red")
+coordination()
 move()
 
 root.mainloop()
